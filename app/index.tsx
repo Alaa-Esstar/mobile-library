42898b89b0d0e86eb83e7dddc4509e0016562e25
Alaa Esstar <alaa.esstar@gmail.com>
2025-06-10 21:22:37 +0100

    nativewind config, new assets

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,17 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, Text, View } from 'react-native'
 import React from 'react'
+import "./global.css"
+
+import Logo from '../assets/img/logo_light.png';
 
 const index = () => {
     return (
-        <View>
-            <Text>Home</Text>
+        <View className='flex-1 items-center justify-center'>
+            <Image source={Logo} className='mx-20' />
+            <Text className='font-bold text-lg'>Home</Text>
+            <Text className='mt-10 mb-32'>Reading list app</Text>
         </View>
     )
 }
 
 export default index
-
-const styles = StyleSheet.create({})
