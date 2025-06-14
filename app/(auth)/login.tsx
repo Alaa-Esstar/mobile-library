d9ce573b20b5287d918bacf28a5e694800f34517
Alaa Esstar <alaa.esstar@gmail.com>
2025-06-14 22:50:36 +0100

    Refactor authentication screens to use react-hook-form for form handling and validation with Zod

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,17 +1,41 @@
 import { View, Text, Pressable, TouchableWithoutFeedback, Keyboard } from 'react-native'
-import { useState } from 'react'
 import { useRouter } from 'expo-router'
 import Button from '../../components/Button';
 import ThemedTextInput from '../../components/ThemedTextInput';
+import { z } from "zod";
+import { useForm, Controller } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from 'react';
+
+const AuthSchema = z.object({
+    email: z.string().email({ message: "Invalid email address" }),
+    password: z.string().min(6, { message: "Password must be at least 6 characters long" }),
+});
+
+type AuthSchemaType = z.infer<typeof AuthSchema>;
 
 const Login = () => {
     const router = useRouter();
 
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
 
-    const handleSubmit = () => {
-        console.log("Login button pressed", email, password);
+    const { control, handleSubmit, formState: { errors } } = useForm<AuthSchemaType>({
+        resolver: zodResolver(AuthSchema),
+        defaultValues: {
+            email: '',
+            password: ''
+        }
+    });
+
+    const onSubmit = (data: AuthSchemaType) => {
+        console.log("Login button pressed", data);
+        try {
+            setIsLoading(true);
+        } catch (error) {
+
+        } finally {
+            setIsLoading(false);
+        }
     }
 
     return (
@@ -19,23 +43,44 @@ const Login = () => {
             <View className='bg-background flex-1 items-center justify-center'>
                 <Text className='text-title text-lg mb-8'>Login to your account</Text>
 
-                <ThemedTextInput
-                    placeholder='Email'
-                    className='w-4/5 mb-5'
-                    keyboardType='email-address'
-                    onChangeText={setEmail}
-                    value={email}
-                />
+                <View className='w-full items-center mb-5'>
+                    <Controller
+                        control={control}
+                        name="email"
+                        render={({ field: { onChange, onBlur, value } }) => (
+                            <ThemedTextInput
+                                className={`w-4/5 ${errors.email ? 'border-red-500' : 'border-bg-300'}`}
+                                placeholder="Enter your email"
+                                keyboardType="email-address"
+                                autoCapitalize="none"
+                                value={value}
+                                onChangeText={onChange}
+                                onBlur={onBlur}
+                            />
+                        )}
+                    />
+                    {errors.email && <Text className="text-red-500 text-sm mt-1">{errors.email.message}</Text>}
+                </View>
+                <View className='w-full items-center mb-5'>
+                    <Controller
+                        control={control}
+                        name="password"
+                        render={({ field: { onChange, onBlur, value } }) => (
+                            <ThemedTextInput
+                                className={`w-4/5 ${errors.password ? 'border-red-500' : 'border-bg-300'}`}
+                                placeholder="Enter your password"
+                                secureTextEntry
+                                autoCapitalize="none"
+                                value={value}
+                                onChangeText={onChange}
+                                onBlur={onBlur}
+                            />
+                        )}
+                    />
+                    {errors.password && <Text className="text-red-500 text-sm mt-1">{errors.password.message}</Text>}
+                </View>
 
-                <ThemedTextInput
-                    placeholder='Password'
-                    className='w-4/5 mb-5'
-                    secureTextEntry
-                    onChangeText={setPassword}
-                    value={password}
-                />
-
-                <Button onPress={handleSubmit}>
+                <Button onPress={handleSubmit(onSubmit)} disabled={isLoading}>
                     <Text className='text-white'>Login</Text>
                 </Button>
 
